eebccb50e36094f772a024a3d87398275e6ddbaf
Yhuri Gross <yhurigross@gmail.com>
2022-07-08 15:46:52 -0300

    Criando o CRUD de tema

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -9,7 +11,11 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
+  tema: Tema = new Tema()
+  listaTema: Tema[]
+
   constructor(
+    private temaService: TemaService,
     private router: Router
   ) { }
 
@@ -18,7 +24,22 @@ export class TemaComponent implements OnInit {
       //alert('Sua seção expirou faça o login novamente.')
       this.router.navigate(['/entrar'])
     }
+    this.findAllTemas()
+  }
 
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[])=>{
+      this.listaTema = resp
+    })
+  }
+
+  cadastrarTema(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+      this.tema = resp
+      alert('tema cadastrado com sucesso')
+      this.findAllTemas()
+      this.tema = new Tema()
+    })
   }
 
 }
